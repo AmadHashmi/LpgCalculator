72ee2f37f01f433dbbe43980a58ab9050caba9a4
Amad Ul Hassan <ah@indigilabs.com>
2025-12-12 00:11:25 +0100

    Fix EAS build: Remove android/ from git, update metro config for RN 0.73+, upgrade CLI to v20.0.0

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+// For React Native 0.73+, ensure proper resolver configuration
+defaultConfig.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
+
+module.exports = defaultConfig;
 
